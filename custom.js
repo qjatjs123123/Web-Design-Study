b42b6fecac3d26eee51e9ca63f706c57a604fa09
Hong Boem Sun <qjatjs123123@naver.com>
2023-09-21 23:58:55 +0900

    plan section 구현

--- custom.js
+++ custom.js
@@ -24,5 +24,14 @@ $(function(){
     }
   })
 
+  // slick.js : history
+  $('.history-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+  })
+  //slick.js : project
 })
 
